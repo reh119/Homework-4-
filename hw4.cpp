9cc30249c72147613fb44019c3d639a9e11a5d92
Rafael Hernandez <reh119@txstate.edu>
2020-09-27 21:38:18 +0000

    Initial commit

---
+++ hw4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "recursive.h"
+
+#include "hw4.h"
+// functions will be in here ALL need to be tail recursive 
+// no loops 
+
+using namespace std; 
+
+int accumulate(list_t l, int (*fn)(int, int), int base)
+{
+  if (list_isEmpty(l)) {
+    return base;
+  } else {
+    return accumulate(list_rest(l), fn, fn(base, list_first(l)));
+  }
+}
+
+int static SumHelper (list_t list, int counter){
+if (list_isEmpty(list)){ // if empty lidt, return current counter
+  return counter; 
+}
+else 
+counter +=list_first(list);
+return SumHelper(list_rest(list),counter); 
+}
+
+
+int sum(list_t list){
+ 
+return SumHelper(list, 0);
+}
+
+
+
+
+// list_first returns an int 
+//list_rest goes down array/list 
+//list_isempty checks to see if array is empty
+
+
